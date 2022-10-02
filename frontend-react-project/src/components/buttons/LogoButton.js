edbcc14008d5048a0ae42dafda8e62a0e9623125
dinuispas <dinuascended@gmail.com>
2022-10-02 18:05:06 +0300

    Co-authored-by: AugustinCibotari <Apsodius1@users.noreply.github.com>

---
+++ frontend-react-project/src/components/buttons/LogoButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LogoButton = () => {
+  return (
+    <div>
+        <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Deutsche_Bank_logo_without_wordmark.svg/1024px-Deutsche_Bank_logo_without_wordmark.svg.png"
+            className="dblogo"
+            alt="logo"
+        />
+    </div>
+  )
+}
+
+export default LogoButton
