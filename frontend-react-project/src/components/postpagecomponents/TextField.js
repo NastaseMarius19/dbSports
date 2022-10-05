c3a2edd2591d15c7d92a61293528729b7363b73f
dinuispas <dinuascended@gmail.com>
2022-10-05 20:58:13 +0300

    POST method for the post page

--- frontend-react-project/src/components/postpagecomponents/TextField.js
+++ frontend-react-project/src/components/postpagecomponents/TextField.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from 'react';
 import { Textarea } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
 // import { extendTheme } from "@chakra-ui/react";
@@ -9,12 +9,30 @@ import PostChoiceButton from "../buttons/PostChoiceButton";
 import UploadPhotoButton from "../buttons/UploadPhotoButton";
 
 function TextField() {
-  let [value, setValue] = React.useState("");
+  const[title, setTitle] = useState('');
+  const[body,setBody] = useState('');
+  const[sport,setSport] = useState('Download');
+  const[isPending, setIsPending] = useState(false);
 
-  let handleInputChange = (e) => {
-    let inputValue = e.target.value;
-    setValue(inputValue);
-  };
+  const handleSubmit= (e) => {
+    e.preventDefault();
+    const sports = {title,  body, sport};
+
+    setIsPending(true);
+
+    fetch('http://localhost:3000/mainpage', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(sports)
+    }).then(() => {
+
+      console.log('new article added');
+      setIsPending(false);
+    })
+  }
+
+
+
   return (
     <div>
       <div className="buttonpostpage">
@@ -25,9 +43,11 @@ function TextField() {
           <DbSports />
         </div>
       </div>
+      <form onSubmit={handleSubmit}>
       <div className="post-content">
         <div>
-        <Text mb="8px" className="posttitle">
+        <Text mb="8px" 
+        className="posttitle">
           Title
         </Text>
         </div>
@@ -35,24 +55,33 @@ function TextField() {
           
         </div>
         <Textarea
-          value={value}
-          onChange={handleInputChange}
           className="textarea"
-          size="sm"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <Text mb="8px" className="posttitle" font-size="22px">
           Text Input
         </Text>
-        <Textarea className="textfield" size="sm" />
+        <Textarea className="textfield" size="sm" 
+        value={body}
+        onChange={(e) => setBody(e.target.value)}/>
         <div>
           <div>
-            <PostChoiceButton />
+            <PostChoiceButton 
+            value = {sport}
+            onChange={(e) => setSport(e.target.value)}/>
           </div>
           <div>
             <UploadPhotoButton />
           </div>
         </div>
+        { !isPending && <button>Post</button>}
+        { isPending && <button>Adding post...</button>}
       </div>
+      </form>
+
+      
+      
     </div>
   );
 }
